31fc37c278ee23c0c229f2abc73867a2a2389cfd
Eralp Kor <eralp.ny@gmail.com>
2020-06-18 12:54:13 -0400

    Setup branch table

---
+++ ls8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+# if len(sys.argv) != 2:
+#     print('USAGE: ls8.py filename')
+#     sys.exit(1)
+
+# program = sys.argv[1]
+program = 'sctest.ls8'
+
+cpu = CPU()
+
+cpu.load(program)
+cpu.run()
+
+
+# print(sys.argv)
