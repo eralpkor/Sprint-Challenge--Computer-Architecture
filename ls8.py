941b87b0a48879f74551d4e02d3f6d6396274eed
eralpkor <eralp.ny@gmail.com>
2020-06-19 11:21:29 -0400

    Clean code

--- ls8.py
+++ ls8.py
@@ -4,14 +4,6 @@ import sys
 import os
 from cpu import *
 
-# if len(sys.argv) != 2:
-#     print("ERR: No file given:\t python ls8.py examples/file_name")
-#     sys.exit(1)
-
-# script_dir = os.path.dirname(__file__)
-# file_name = sys.argv[1]
-# arg handed should be a file path ex: examples/file_name
-# file_path = os.path.join(script_dir, file_name)
 
 if len(sys.argv) != 2:
     print('Usage: ls8.py filename')
